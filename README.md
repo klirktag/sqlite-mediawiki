6d877177c94d479f7bee16d89f89cfe67a985c45
klirktag <klirktag@protonmail.com>
2023-08-24 22:45:42 +0200

    adding mounts

---
+++ README.md
@@ -0,0 +1,6 @@
+Based on docker-compose here
+https://hub.docker.com/_/mediawiki
+
+# image admin links
+http://localhost:8080/index.php/Special:Upload
+http://localhost:8080/index.php/Special:ListFiles
---
+++ TemplateStyles/vendor/composer/installed.php
@@ -0,0 +1,60 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'cdf395d8d0761d1586b0828273dd88b99879fd4b',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'cdf395d8d0761d1586b0828273dd88b99879fd4b',
+    ),
+    'wikimedia/at-ease' => 
+    array (
+      'pretty_version' => 'v2.1.0',
+      'version' => '2.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e8ebaa7bb7c8a8395481a05f6dc4deaceab11c33',
+    ),
+    'wikimedia/css-sanitizer' => 
+    array (
+      'pretty_version' => 'v4.0.1',
+      'version' => '4.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e8cd197f9c47da649fff3dad4c74c3d85276c208',
+    ),
+    'wikimedia/scoped-callback' => 
+    array (
+      'pretty_version' => 'v4.0.0',
+      'version' => '4.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'dd522a6cfb4c0180b0a39d12fc57046a347c4d81',
+    ),
+    'wikimedia/utfnormal' => 
+    array (
+      'pretty_version' => '3.0.2',
+      'version' => '3.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e690d29487a6ee346bcf4cbf5a6fd89170fa2306',
+    ),
+  ),
+);
